2163cc4f185f8ac5fdd2f76ad6646dbd0020b7f7
William Fuller <william.fuller@instem.com>
2022-02-24 16:49:43 +0100

    feat: added some OO goodness

---
+++ example.py
@@ -0,0 +1,4 @@
+from wordle_game import WorldGame
+
+game = WorldGame(total_rounds=6)
+game.play_full_game()
--- wordle_game.py
+++ wordle_game.py
@@ -2,52 +2,68 @@ import requests
 import random
 import re
 
-def is_letter_correct(letter, chosen_word):
-    return letter in chosen_word
+WORD_LIST_URL = "https://raw.githubusercontent.com/tabatkins/wordle-list/main/words"
 
-def is_letter_in_correct_position(index, guess, chosen_word):
-    return guess[index] == chosen_word[index]
+class WorldGame():
+    def __init__(self, total_rounds=6):
+        self.allowed_words = self.__get_allowed_word_list()
+        self.chosen_word = self.__choose_word(self.allowed_words)
+        self.total_rounds = total_rounds
+        self.round_count = 0
+
+    # private methods
+    def __get_allowed_word_list(self):
+        return requests.get(WORD_LIST_URL).text.split("\n")
+
+    def __choose_word(self, word_list):
+        return random.choice(word_list)
+
+    def __is_letter_correct(self,letter, chosen_word):
+        return letter in chosen_word
+
+    def __is_letter_in_correct_position(self, index, guess, chosen_word):
+        return guess[index] == chosen_word[index]
+
+    # public methods
+    def play_full_game(self):
+        guess = ""
+        clue_str = "XXXXX"
+        is_correct_guess = False
+        for guess_count in range(self.total_rounds):
+            while(not(re.search(r'^[a-z]{5}$', guess, re.IGNORECASE) and (guess in self.allowed_words))):
+                guess = input("Enter a guess (word my be 5 long and contain only A-Z case-insensitive):")
+            
+            guess = guess.lower()
+            clue_list = list(clue_str)
+            for i in range(5):
+
+                guess_letter_check_output = "X"
+
+                if(self.__is_letter_correct(guess[i], self.chosen_word)):
+                    guess_letter_check_output = "_"
+
+                if(guess_letter_check_output == "_" and self.__is_letter_in_correct_position(i, guess, self.chosen_word)):
+                    guess_letter_check_output = "O"
+                
+                clue_list[i] = guess_letter_check_output
+
+            clue_str = "".join(clue_list)
+
+            print(guess)
+            print(clue_str)
+
+            if(guess == self.chosen_word):
+                is_correct_guess = True
+                break
+
+            guess = ""
+            clue_str = "XXXXX"
+
+        if(is_correct_guess):
+            print(f"You guess the correct word {self.chosen_word} in {guess_count + 1} tries WELL DONE!!!")
+        else:
+            print(f"the correct guess was {self.chosen_word}, better luck next time")
+
+            
+
     
-wordle_word_list_url = "https://raw.githubusercontent.com/tabatkins/wordle-list/main/words"
-wordle_word_list = requests.get(wordle_word_list_url).text.split("\n")
-
-chosen_word = random.choice(wordle_word_list)
-
-guess = ""
-clue_str = "XXXXX"
-is_correct_guess = False
-for guess_count in range(6):
-    while(not(re.search(r'^[a-z]{5}$', guess, re.IGNORECASE) and (guess in wordle_word_list))):
-        guess = input("Enter a guess (word my be 5 long and contain only A-Z):")
-    
-    guess = guess.lower()
-
-    clue_list = list(clue_str)
-    for i in range(5):
-
-        guess_letter_check_output = "X"
-
-        if(is_letter_correct(guess[i], chosen_word)):
-            guess_letter_check_output = "_"
-
-        if(guess_letter_check_output == "_" and is_letter_in_correct_position(i, guess, chosen_word)):
-            guess_letter_check_output = "O"
-        
-        clue_list[i] = guess_letter_check_output
-
-    clue_str = "".join(clue_list)
-
-    print(guess)
-    print(clue_str)
-
-    if(guess == chosen_word):
-        is_correct_guess = True
-        break
-
-    guess = ""
-    clue_str = "XXXXX"
-
-if(is_correct_guess):
-    print(f"You guess the correct word {chosen_word} in {guess_count} tries WELL DONE!!!")
-else:
-    print(f"the correct guess was {chosen_word}, better luck next time")
